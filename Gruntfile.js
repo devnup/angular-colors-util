b5212b415c73289354d70d5cfbac4ba3b34dc529
Luis Eduardo Brito <luiseduardo14@gmail.com>
2015-04-09 18:35:34 -0300

    gruntfile from boilerplate

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,104 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
+  // Get snippet information from package.json
+  var pkg = grunt.file.readJSON('package.json');
 
-        pkg: grunt.file.readJSON('package.json'),
+  // Fancy way to always create a file as as [package[name]].css
+  var constructor = function (origin, dest) {
+    var obj = {};
+    obj[dest] = [origin];
+    return obj;
+  };
 
-        copy: {
-            main: {
-                src: 'lib/src/angular-colors-util.js',
-                dest: 'lib/dist/js/angular-colors-util.js'
-            }
+  grunt.initConfig({
+
+    pkg: pkg,
+
+    concat: {
+      lib: {
+        src: ['lib/src/js/**.js'],
+        dest: 'lib/dist/js/' + pkg.name + '.js'
+      }
+    },
+
+    uglify: {
+      minify: {
+        files: (function () {
+
+          // Files path
+          var origin = 'lib/dist/js/**.js';
+          var dest = 'lib/dist/js/' + pkg.name + '.min.js';
+
+          // Fancy way to always create a file as as [package[name]].css
+          return constructor(origin, dest);
+
+        })()
+      }
+    },
+
+    less: {
+
+      lib: {
+        options: {
+          paths: ['lib/src/less']
         },
+        files: (function () {
 
-        uglify: {
-            minify: {
-                files: {
-                    'lib/dist/js/angular-colors-util.min.js': ['lib/dist/js/angular-colors-util.js']
-                }
-            }
+          // Files path
+          var origin = 'lib/src/less/**.less';
+          var dest = 'lib/dist/css/' + pkg.name + '.css';
+
+          // Fancy way to always create a file as as [package[name]].css
+          return constructor(origin, dest);
+
+        })()
+      },
+
+      assets: {
+        options: {
+          paths: ['lib/assets/less']
         },
-
-        less: {
-            development: {
-                options: {
-                    paths: ['lib/assets/less']
-                },
-                files: {
-                    'lib/assets/css/style.css': 'lib/assets/less/**.less'
-                }
-            }
-        },
-
-        cssmin: {
-            target: {
-                files: {
-                    'lib/assets/css/style.min.css': ['lib/dist/css/style.css']
-                }
-            }
-        },
-
-        clean: ["lib/dist/", "lib/assets/css/"],
-
-        watch: {
-            files: ['lib/**'],
-            tasks: ['dev']
+        files: {
+          'lib/assets/css/style.css': 'lib/assets/less/**.less'
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    cssmin: {
+      lib: {
+        files: (function () {
 
-    grunt.registerTask('dev', ['clean', 'copy', 'uglify', 'less', 'cssmin']);
-    grunt.registerTask('default', ['dev']);
+          // Files path
+          var origin = 'lib/dist/css/' + pkg.name + '.css';
+          var dest = 'lib/dist/css/' + pkg.name + '.min.css';
+
+          // Fancy way to always create a file as as [package[name]].css
+          return constructor(origin, dest);
+
+        })()
+      },
+      assets: {
+        files: {
+          'lib/assets/css/style.min.css': ['lib/assets/css/style.css']
+        }
+      }
+    },
+
+    clean: ["lib/dist/", "lib/assets/css/"],
+
+    watch: {
+      files: ['lib/**'],
+      tasks: ['dev']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('dev', ['clean', 'concat', 'uglify', 'less', 'cssmin']);
+  grunt.registerTask('default', ['dev']);
 };
